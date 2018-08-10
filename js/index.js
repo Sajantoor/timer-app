6a7146cc1cd0cd3f429567bba1da0404bb9b680a
Sajan Toor <Sajantoor98@gmail.com>
2018-08-09 22:45:40 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var startButton = document.getElementById('start');
+startButton.addEventListener('click', startTimer);
+
+function startTimer() {
+  var startText = document.getElementById('start-text');
+}
