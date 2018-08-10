e6fab3c3134d9af2556a57ecf5d4e1cc274daba1
Sajantoor98 <Sajantoor98@gmail.com>
2018-08-10 00:00:33 -0700

    Started the stuff

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
 var startButton = document.getElementById('start');
-startButton.addEventListener('click', startTimer);
+startButton.onclick = function() {
+  startTimer();
+}
 
 function startTimer() {
   var startText = document.getElementById('start-text');
+
+  if (startText.innerText == "Start") {
+    startText.innerText = "Stop";
+    // Start the actual timer.
+    countDown(1500);
+  } else if (startText.innerText == "Stop") {
+    startText.innerText = "Start";
+    // Add a button for reseting the timer when stopped
+  }
+}
+
+function countDown(time, input) {
+  var timer = document.getElementById('timer');
+
+  x = setInterval(countDownInterval, 1000)
+
+  function countDownInterval() {
+    if (input = true) {
+      minutes = Math.floor(time / 60);
+      seconds = time % 60;
+
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      timer.innerText = minutes + ":" + seconds;
+
+      time--;
+      if (time <= 0) {
+        clearInterval(x);
+      }
+    }
+  }
 }
